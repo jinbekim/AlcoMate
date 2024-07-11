0b47e1d0e3f33b96d7e904f383df260d35249171
김진범 <jinbekim@ex-em.com>
2024-07-11 19:30:16 +0900

    add: widmark formula

---
+++ src/domain/calculateBAC.ts
@@ -0,0 +1,32 @@
+/**
+ * Widmark Formula
+ */
+const 시간당분해되는알콜비율 = 0.017;
+
+export type BacVariables = {
+  alcoholConsumed: number;
+  weight: number
+  gender: 'male' | 'female'
+  hours: number
+}
+
+export const 분포계수 = (gender: 'male' | 'female') => {
+  return gender === 'male' ? 0.68 : 0.55;
+}
+
+export const calculateBacValue = ({
+  alcoholConsumed,
+  weight,
+  gender,
+  hours,
+
+}: BacVariables) => {
+  const bacValue = (alcoholConsumed / (weight * 분포계수(gender))) - (시간당분해되는알콜비율 * hours);
+
+  return bacValue;
+}
+
+export const getMetabolizedAlcohol = ({ weight, gender }: BacVariables) => {
+  const metabolizedAlcohol = 시간당분해되는알콜비율 * weight * 분포계수(gender) * (1 / 60);
+  return metabolizedAlcohol;
+}
