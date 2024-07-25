6b56932f450cefaa069cfc0a0700bc4ee1c14e06
김진범 <jinbekim@ex-em.com>
2024-07-25 22:01:54 +0900

    feat: alcoholGauge

--- src/present/AlcoholGauge.tsx
+++ src/present/AlcoholGauge.tsx
@@ -1,15 +1,59 @@
 import { 음주운전기준 } from '@/domain/lawStandard';
 import { Gender } from '@/domain/types';
+import { useEffect, useMemo, useState } from 'react';
 
 interface AlcoholGaugeProps {
 	estimatedBac: string;
 	gender: Gender;
 }
 
+const Gauge = ({
+	percentage,
+	color,
+}: {
+	percentage: number;
+	color: string;
+}) => {
+	const [value, setValue] = useState(0);
+
+	useEffect(() => {
+		let timer: number;
+		if (value < percentage) {
+			timer = window.setTimeout(() => {
+				setValue(value + 1);
+			}, 30);
+		}
+		return () => {
+			clearTimeout(timer);
+		};
+	}, [value, percentage]);
+
+	return (
+		<div
+			className={`h-2.5 ${color}`}
+			style={{ width: `${value % 100}%` }}
+		></div>
+	);
+};
+
 export const AlcoholGauge = ({ estimatedBac }: AlcoholGaugeProps) => {
-	const value = +estimatedBac % 음주운전기준;
-	const idx = Math.floor(+estimatedBac / 음주운전기준);
-	const color = ['bg-green-500', 'bg-yellow-500', 'bg-red-500'][idx]; // TODO: 최소 10개
+	const percentage = useMemo(
+		() => (+estimatedBac / 음주운전기준) * 100,
+		[estimatedBac],
+	);
+	const idx = useMemo(() => Math.floor(percentage / 100), [percentage]);
+	const color = [
+		'bg-green-700',
+		'bg-yellow-400',
+		'bg-yellow-500',
+		'bg-red-500',
+		'bg-red-600',
+		'bg-red-700',
+		'bg-red-800',
+		'bg-red-900',
+		'bg-red-900',
+		'bg-gray-900', // 0.3 이상 이면 거의 치사량
+	][idx];
 
 	return (
 		<div className="flex flex-col items-center justify-center p-4">
@@ -18,10 +62,7 @@ export const AlcoholGauge = ({ estimatedBac }: AlcoholGaugeProps) => {
 				<span className="text-sm font-medium">{estimatedBac}</span>
 			</div>
 			<div className="w-full bg-gray-200 h-2.5">
-				<div
-					className={`h-2.5 ${color} transition-transform`}
-					style={{ width: `${(value / 음주운전기준) * 100}%` }}
-				></div>
+				<Gauge percentage={percentage} color={color} />
 			</div>
 		</div>
 	);
